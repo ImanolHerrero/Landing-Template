aacc12a9512385ef9c8b38f2d7492f99c90bb6c1
Imanol <imano.arias@gmail.com>
2025-03-07 22:08:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Contact01Page from "./components/contact-01/contact-01";
+import FAQ01 from "./components/faq-01/faq-01";
+import Features02Page from "./components/features-02/features-02";
+import Footer05Page from "./components/footer-05/footer-05";
+import Hero02 from "./components/hero-02/hero-02";
+import Navbar01Page from "./components/navbar-01/navbar-01";
+
+function App() {
+  return (
+    <>
+      <Navbar01Page />
+      <Hero02 />
+      <Features02Page />
+      <FAQ01 />
+      <Contact01Page />
+      <Footer05Page />
+    </>
+  );
+}
+
+export default App;
